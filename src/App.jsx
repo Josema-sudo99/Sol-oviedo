896b3b94afef2d94ba3ad6cae9e1be50be142a13
Josema-sudo99 <josemmm010203@gmail.com>
2023-10-12 23:20:11 -0500

    sol

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="love-img">
+            <h1 className="love-text">para la chica mas hermosa</h1>
+            <p className="php">🍀💝💝🍀💝💝🍀</p>
+            <p className="php">💝💝💝💝💝💝💝</p>
+            <p className="php">🍀💝💝💝💝💝🍀</p>
+            <p className="php">🍀🍀💝💝💝🍀🍀</p>
+            <p className="php">🍀🍀🍀💝🍀🍀🍀</p>
+            <a href="src\Card\loveCard.html">
+                <button class="next">▶️</button>
+            </a>
+        </div>
+    </>
+  )
+}
+
+export default App
